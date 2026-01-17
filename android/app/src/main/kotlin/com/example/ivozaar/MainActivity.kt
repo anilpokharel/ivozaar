8620f501f1045b7730cd6a44049cb4d44cd95826
Anil pokhrel <154496058+anilpokharel@users.noreply.github.com>
2026-01-17 17:47:53 +0545

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/ivozaar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ivozaar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome To the Flutter App(IVOzaar)'),
+      ), //AppBar
+      body: Center(
+        child: Container(
+        width: 100,
+        height: 100,
+        color: const Color.fromARGB(255, 8, 60, 149),
+        child: const Text('Hello Developers'),
+      ),
+      ),
+
+    );
+  }
+}
